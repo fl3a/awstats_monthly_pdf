501f387392ca49b85979e0d4ba6972fcbd315c41
Florian Latzel <florian.latzel@gmail.com>
2023-02-27 00:03:08 +0100

    Fixed year and put quotes around subject.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -17,10 +17,10 @@ htmldoc="/home/kdoz/bin/htmldoc"
 
 mail="/usr/bin/mail"
 email="florian@latzel.io"
-subject="AWStats monthly report `date +Y.%m` for $domain"
+subject="AWStats monthly report `date +%Y.%m` for $domain"
 pdf="${dir}/awstats.${domain}.pdf"
 
 perl $buildstaticpages -config=$domain -output -staticlinks \
 	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc && \
-echo $subject | mail -s $subject -A $pdf $email && \
+echo "$subject" | mail -s "$subject" -A $pdf $email && \
 rm  $pdf
