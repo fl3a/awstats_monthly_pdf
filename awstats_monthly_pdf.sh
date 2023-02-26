1252b9b2d1d66a215d8e44fe58d5212c5d4a1d78
Florian Latzel <florian.latzel@gmail.com>
2023-02-26 22:09:19 +0100

    Removed if statement.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-#if [ $(date -d +1day +%d) -eq 1 ]; then
-if [ true ]; then
-	perl /home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl \
-		-config=florian.latzel.io -output -staticlinks \
-		-awstatsprog=/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl \
-		-lang=de -dir=/home/kdoz/html -buildpdf=/home/kdoz/bin/htmldoc && \
-	echo "AWStats Bericht `date +%Y.%m`" |  \
-	mailx -s "AWStats montly" \
-		-a /home/kdoz/html/awstats.florian.latzel.io.pdf florian@latzel.io && \
-	rm /home/kdoz/html/awstats.florian.latzel.io.pdf
-fi
+# Exit when today + 1 day not equals 1, 
+# which means today is not the last day in the month
+[ $(date -d +1day +%d) -ne 1 ] && exit 0
+
+# Generate pdf
+perl /home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl \
+	-config=florian.latzel.io -output -staticlinks \
+	-awstatsprog=/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl \
+	-lang=de -dir=/home/kdoz/html -buildpdf=/home/kdoz/bin/htmldoc && \
+# Send pdf via mail
+echo "AWStats Bericht `date +%Y.%m`" |  \
+mailx -s "AWStats montly" \
+	-a /home/kdoz/html/awstats.florian.latzel.io.pdf florian@latzel.io && \
+rm /home/kdoz/html/awstats.florian.latzel.io.pdf
 
