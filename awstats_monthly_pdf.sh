3993904db5d572524b546c829f902a4eda344439
Florian Latzel <florian.latzel@gmail.com>
2023-02-26 23:58:18 +0100

    Put everything in variables.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+# Generate awstats pdf on last day of month 
+# and send  the pdf as attachment via email
+
 # Exit when today + 1 day not equals 1, 
 # which means today is not the last day in the month
 [ $(date -d +1day +%d) -ne 1 ] && exit 0
 
-# Generate pdf
-perl /home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl \
-	-config=florian.latzel.io -output -staticlinks \
-	-awstatsprog=/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl \
-	-lang=de -dir=/home/kdoz/html -buildpdf=/home/kdoz/bin/htmldoc && \
-# Send pdf via mail
-echo "AWStats Bericht `date +%Y.%m`" |  \
-mailx -s "AWStats montly" \
-	-a /home/kdoz/html/awstats.florian.latzel.io.pdf florian@latzel.io && \
-rm /home/kdoz/html/awstats.florian.latzel.io.pdf
+set -e
 
+buildstaticpages="/home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl"
+awstats="/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl"
+domain="florian.latzel.io"
+dir="/home/kdoz/html"
+htmldoc="/home/kdoz/bin/htmldoc"
+
+mail="/usr/bin/mail"
+email="florian@latzel.io"
+subject="AWStats monthly report `date +Y.%m` for $domain"
+pdf="${dir}/awstats.${domain}.pdf"
+
+perl $buildstaticpages -config=$domain -output -staticlinks \
+	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc && \
+echo $subject | mail -s $subject -A $pdf $email && \
+rm  $pdf
