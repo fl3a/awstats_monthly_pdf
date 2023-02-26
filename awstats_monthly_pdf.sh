d387350e7331e0c5b867258cb2f8bf47253cf4ee
Florian Latzel <florian.latzel@gmail.com>
2023-02-27 00:19:54 +0100

    Switched to mailx.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -7,6 +7,7 @@
 # which means today is not the last day in the month
 [ $(date -d +1day +%d) -ne 1 ] && exit 0
 
+set -x
 set -e
 
 buildstaticpages="/home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl"
@@ -15,12 +16,11 @@ domain="florian.latzel.io"
 dir="/home/kdoz/html"
 htmldoc="/home/kdoz/bin/htmldoc"
 
-mail="/usr/bin/mail"
 email="florian@latzel.io"
 subject="AWStats monthly report `date +%Y.%m` for $domain"
 pdf="${dir}/awstats.${domain}.pdf"
 
 perl $buildstaticpages -config=$domain -output -staticlinks \
 	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc && \
-echo "$subject" | mail -s "$subject" -A $pdf $email && \
+echo "$subject" | mailx -s "$subject" -a $pdf $email && \
 rm  $pdf
