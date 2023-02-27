1ce283a9cefdf2dbc51dd03a878d3e55f69a1971
Florian Latzel <florian.latzel@gmail.com>
2023-02-27 09:28:09 +0100

    Removed && execution since we use set -x.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Generate awstats pdf on last day of month 
-# and send  the pdf as attachment via email
+# and send  the pdf as attachment via email (mailx)
 
 # Exit when today + 1 day not equals 1, 
 # which means today is not the last day in the month
-[ $(date -d +1day +%d) -ne 1 ] && exit 0
+[ $(date -d +1day +%d) -ne 1 ] && exit 
 
-set -x
-set -e
+set -ue
 
 buildstaticpages="/home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl"
 awstats="/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl"
@@ -21,6 +20,6 @@ subject="AWStats monthly report `date +%Y.%m` for $domain"
 pdf="${dir}/awstats.${domain}.pdf"
 
 perl $buildstaticpages -config=$domain -output -staticlinks \
-	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc && \
-echo "$subject" | mailx -s "$subject" -a $pdf $email && \
+	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc 
+echo "$subject" | mailx -s "$subject" -a $pdf $email 
 rm  $pdf
