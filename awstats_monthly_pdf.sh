9cd7623a7308c1899cbbe6de0f369485266bacd3
Florian Latzel <florian.latzel@gmail.com>
2023-02-27 09:32:22 +0100

    Added lang variable.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -14,12 +14,13 @@ awstats="/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl"
 domain="florian.latzel.io"
 dir="/home/kdoz/html"
 htmldoc="/home/kdoz/bin/htmldoc"
+lang="de"
 
 email="florian@latzel.io"
 subject="AWStats monthly report `date +%Y.%m` for $domain"
 pdf="${dir}/awstats.${domain}.pdf"
 
 perl $buildstaticpages -config=$domain -output -staticlinks \
-	-awstatsprog=$awstats -lang=de -dir=$dir -buildpdf=$htmldoc 
+	-awstatsprog=$awstats -lang=$lang -dir=$dir -buildpdf=$htmldoc 
 echo "$subject" | mailx -s "$subject" -a $pdf $email 
 rm  $pdf
