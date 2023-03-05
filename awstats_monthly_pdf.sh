c3a66873bc25364d04d99efc7fce32b5f14dd8d7
Florian Latzel <florian.latzel@gmail.com>
2023-03-05 13:24:02 +0100

    Minor.

--- awstats_monthly_pdf.sh
+++ awstats_monthly_pdf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Generate awstats pdf on last day of month 
-# and send  the pdf as attachment via email (mailx)
-# https://github.com/fl3a/awstats_monthly_pdf
+# and send  the pdf as attachment via email (mailx).
+# See https://github.com/fl3a/awstats_monthly_pdf
 
 set -o nounset
 set -o errexit
@@ -12,15 +12,16 @@ set -o errexit
 [ $(date -d +1day +%d) -ne 1 ] && exit 
 
 buildstaticpages="/home/kdoz/bin/awstats-7.8/tools/awstats_buildstaticpages.pl"
-awstats="/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl"
 domain="florian.latzel.io"
+config="awstats.florian.latzel.io.conf"
+awstats="/home/kdoz/bin/awstats-7.8/wwwroot/cgi-bin/awstats.pl"
+lang="de"
 dir="/home/kdoz/html"
 htmldoc="/home/kdoz/bin/htmldoc"
-lang="de"
 
-email="florian@latzel.io"
 subject="Statistik für $domain (`date +%Y.%m`)"
 pdf="${dir}/awstats.${domain}.pdf"
+email="florian@latzel.io"
 
 perl $buildstaticpages -config=$domain -output -staticlinks \
 	-awstatsprog=$awstats -lang=$lang -dir=$dir -buildpdf=$htmldoc 
